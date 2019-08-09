bf6e2376583f3a5c841595a33523eafe163069e9
Kanishkar J <kanishkarj@hotmail.com>
2019-08-10 02:57:29 +0530

    Code Refactor

--- example_crates/qemu-m4/src/main.rs
+++ example_crates/qemu-m4/src/main.rs
@@ -24,6 +24,14 @@ fn main() -> ! {
         }
         block_unblock(1, true);
         block_unblock(2, false);
+        block_unblock(3, true);
+    });
+    let _ = create_task(2, &mut stack2, || loop {
+        for _ in 0..5 {
+            let _ = hprintln!("in user task 2 !!");
+        }
+        block_unblock(1, true);
+        block_unblock(2, true);
         block_unblock(3, false);
     });
     let _ = create_task(3, &mut stack3, || loop {
@@ -31,16 +39,8 @@ fn main() -> ! {
             let _ = hprintln!("in user task 3 !!");
         }
         block_unblock(1, false);
-        block_unblock(2, false);
-        block_unblock(3, true);
-    });
-    let _ = create_task(2, &mut stack2, || loop {
-        for _ in 0..5 {
-            let _ = hprintln!("in user task 2 !!");
-        }
-        block_unblock(1, false);
         block_unblock(2, true);
-        block_unblock(3, false);
+        block_unblock(3, true);
     });
     release(&[1, 2, 3]);
 
