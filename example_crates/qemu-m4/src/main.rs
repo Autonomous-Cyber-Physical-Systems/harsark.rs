38e887d396b733299f79dafba27097041c850795
Kanishkar J <kanishkarj@hotmail.com>
2019-08-10 02:35:46 +0530

    Tasks as array of options

--- example_crates/qemu-m4/src/main.rs
+++ example_crates/qemu-m4/src/main.rs
@@ -2,8 +2,8 @@
 #![no_main]
 
 extern crate panic_semihosting;
+use cortex_m::interrupt::{disable, enable};
 use cortex_m::peripheral::syst::SystClkSource;
-use cortex_m::interrupt::{enable,disable};
 use cortex_m_rt::entry;
 use cortex_m_semihosting::hprintln;
 use cortexm_threads::*;
@@ -18,43 +18,31 @@ fn main() -> ! {
     let mut stack2 = [0xDEADBEEF; 512];
     let mut stack3 = [0xDEADBEEF; 512];
 
-    let _ = create_task(
-        1,
-        &mut stack1,
-        || loop {
+    let _ = create_task(1, &mut stack1, || loop {
         for _ in 0..5 {
-        let _ = hprintln!("in user task 1 !!");
+            let _ = hprintln!("in user task 1 !!");
         }
         block_unblock(1, true);
         block_unblock(2, false);
         block_unblock(3, false);
-    }
-    );
-    let _ = create_task(
-        3,
-        &mut stack3,
-                        || loop {
+    });
+    let _ = create_task(3, &mut stack3, || loop {
         for _ in 0..5 {
-        let _ = hprintln!("in user task 3 !!");
+            let _ = hprintln!("in user task 3 !!");
         }
         block_unblock(1, false);
         block_unblock(2, false);
         block_unblock(3, true);
-    }
-    );
-    let _ = create_task(
-        2,
-        &mut stack2,
-        || loop {
-            for _ in 0..5 {
+    });
+    let _ = create_task(2, &mut stack2, || loop {
+        for _ in 0..5 {
             let _ = hprintln!("in user task 2 !!");
-            }
-            block_unblock(1, false);
-            block_unblock(2, true);
-            block_unblock(3, false);
         }
-    );
-    release(&[1,2,3]);
+        block_unblock(1, false);
+        block_unblock(2, true);
+        block_unblock(3, false);
+    });
+    release(&[1, 2, 3]);
 
     unsafe {
         enable();
@@ -62,6 +50,5 @@ fn main() -> ! {
 
     init();
 
-    loop {
-    }
+    loop {}
 }
