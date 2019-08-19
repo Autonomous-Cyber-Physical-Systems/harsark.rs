32addb335cd61017b55b50d2cb99db7e0817ba87
Kanishkar J <kanishkarj@hotmail.com>
2019-08-19 16:33:41 +0530

    develop the spawn! macro to create tasks

--- example_crates/qemu-m4/src/main.rs
+++ example_crates/qemu-m4/src/main.rs
@@ -6,12 +6,11 @@ use cortex_m::interrupt::{disable, enable};
 use cortex_m::peripheral::syst::SystClkSource;
 use cortex_m_rt::entry;
 use cortex_m_semihosting::hprintln;
-
 use cortexm_threads::event_manager::*;
 use cortexm_threads::messaging::*;
 use cortexm_threads::resource_management::*;
 use cortexm_threads::semaphores::*;
-use cortexm_threads::task;
+use cortexm_threads::spawn;
 use cortexm_threads::task_manager::*;
 
 #[entry]
@@ -20,27 +19,22 @@ fn main() -> ! {
     let mut stack2 = [0; 512];
     let mut stack3 = [0; 512];
 
-    set_permitted_tasks(1, 6);
+    set_permitted_tasks(1,6);
 
-    let _ = task!(1, &mut stack1, {
-        loop {
+    spawn!(thread1, 1, {
             for _ in 0..5 {
                 let _ = hprintln!("in user task 1 !!");
             }
-            lock(1);
-            release(&4);
-        }
     });
-    let _ = task!(2, &mut stack2, {
-        loop {
+
+    spawn!(thread2, 2, {
             for _ in 0..5 {
                 let _ = hprintln!("in user task 2 !!");
             }
-        }
     });
 
+//    release_tasks(&[1]);
     release(&2);
-
     init(true);
     start_kernel();
 
