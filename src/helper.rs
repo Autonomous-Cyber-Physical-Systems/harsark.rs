f9ce220a261538641e60c7855178cbe60593d375
Kanishkar J <kanishkarj@hotmail.com>
2019-08-27 15:46:46 +0530

    Move resource management data structures to kernel submodule

--- src/helper.rs
+++ src/helper.rs
@@ -1,7 +1,20 @@
+use crate::config::MAX_TASKS;
+
 pub fn generate_task_mask(tasks: &[u32]) -> u32 {
     let mut task_mask = 0;
     for tid in tasks {
         task_mask |= 1 << *tid;
     }
     task_mask
+}
+
+pub fn get_msb(val: &u32) -> usize {
+    for i in (0..MAX_TASKS).rev() {
+        let mut mask = 0;
+        mask <<= i;
+        if val & mask == mask {
+            return i;
+        }
+    }
+    return 0;
 }
--- src/resource_management.rs
+++ src/resource_management.rs
@@ -1,67 +1,8 @@
-use crate::config::MAX_TASKS;
-use crate::event_manager::execute_event;
-use crate::task_manager::{block_tasks, preempt, unblock_tasks};
-use core::cmp::max;
-use core::pin::Pin;
-use cortex_m_semihosting::hprintln;
+use core::cell::RefCell;
+use cortex_m::interrupt::Mutex;
 
-const PI: u32 = 0;
-static mut RCB: [u32; MAX_TASKS] = [PI; MAX_TASKS];
+use crate::kernel::resource_management::ResourceManager;
 
-static mut TOP: usize = 0;
-static mut PI_STACK: [u32; MAX_TASKS] = [0; MAX_TASKS];
-static mut SYSTEM_CEILING: u32 = PI;
-
-pub fn set_permitted_tasks(id: usize, tasks_mask: u32) {
-    for tid in (0..MAX_TASKS).rev() {
-        unsafe {
-            let mask = 1 << tid as u32;
-            if tasks_mask & mask == mask {
-                RCB[id] = max(RCB[id], mask);
-            }
-        }
-    }
-}
-
-pub fn lock(id: usize) {
-    let rt_ceiling = unsafe { RCB[id] };
-    if rt_ceiling > unsafe { SYSTEM_CEILING } {
-        push_stack(rt_ceiling);
-        let mut mask = 0;
-        for i in 0..(rt_ceiling - 1) {
-            mask |= 1 << i;
-        }
-        unsafe {
-            SYSTEM_CEILING = RCB[id];
-            hprintln!("{}", mask);
-            block_tasks(mask);
-        }
-    }
-}
-
-pub fn unlock(id: usize) {
-    let rt_ceiling = unsafe { RCB[id] };
-    if rt_ceiling == unsafe { SYSTEM_CEILING } {
-        pop_stack();
-        let mut mask = 0;
-        for i in 0..(rt_ceiling - 1) {
-            mask &= 1 << i;
-        }
-        unblock_tasks(mask);
-        preempt();
-    }
-}
-
-fn pop_stack() {
-    unsafe {
-        SYSTEM_CEILING = PI_STACK[TOP - 1];
-        TOP -= 1;
-    }
-}
-
-fn push_stack(ceiling: u32) {
-    unsafe {
-        PI_STACK[TOP] = SYSTEM_CEILING;
-        TOP += 1;
-    }
+lazy_static!{
+    static ref Resources: ResourceManager = ResourceManager::init();
 }
