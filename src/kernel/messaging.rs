6521e60489c83d5f777e2d55a21c61bf0e3394a7
Kanishkar J <kanishkarj@hotmail.com>
2019-08-30 16:16:04 +0530

    Rust fmt

--- src/kernel/messaging.rs
+++ src/kernel/messaging.rs
@@ -1,11 +1,11 @@
 //use core::alloc::
 use crate::config::{MAX_BUFFER_SIZE, MAX_TASKS, SEMAPHORE_COUNT};
 use crate::errors::KernelError;
-use crate::kernel::semaphores::{Semaphores, SemaphoreControlBlock, SemaphoreId};
+use crate::kernel::semaphores::{SemaphoreControlBlock, SemaphoreId, Semaphores};
 use crate::kernel::task_manager::{get_RT, release};
 
-use cortex_m_semihosting::hprintln;
 use crate::helper::generate_task_mask;
+use cortex_m_semihosting::hprintln;
 
 pub type StaticBuffer = &'static [u32];
 
@@ -24,11 +24,11 @@ pub struct MCB {
 pub struct MessagingManager {
     tcb_table: [TCB; MAX_TASKS],
     mcb_table: [MCB; SEMAPHORE_COUNT],
-    msg_scb_table: Semaphores
+    msg_scb_table: Semaphores,
 }
 
 impl<'a> MessagingManager {
-    pub fn new () -> Self{
+    pub fn new() -> Self {
         Self {
             tcb_table: [TCB {
                 dest_buffer: [0; MAX_TASKS],
@@ -40,19 +40,20 @@ impl<'a> MessagingManager {
             }; SEMAPHORE_COUNT],
             msg_scb_table: Semaphores {
                 table: [SemaphoreControlBlock { flags: 0, tasks: 0 }; SEMAPHORE_COUNT],
-                curr: 0
-            }
+                curr: 0,
+            },
         }
     }
 
     pub fn broadcast(&mut self, sem_id: SemaphoreId) -> Result<(), KernelError> {
-            if self.mcb_table.get(sem_id).is_none() {
-                return Err(KernelError::NotFound);
-            }
-            let mcb = self.mcb_table[sem_id];
-            self.copy_msg(sem_id)?;
-            self.msg_scb_table.signal_and_release(sem_id, &mcb.receivers)?;
-            return Ok(());
+        if self.mcb_table.get(sem_id).is_none() {
+            return Err(KernelError::NotFound);
+        }
+        let mcb = self.mcb_table[sem_id];
+        self.copy_msg(sem_id)?;
+        self.msg_scb_table
+            .signal_and_release(sem_id, &mcb.receivers)?;
+        return Ok(());
     }
 
     fn copy_msg(&mut self, sem_id: SemaphoreId) -> Result<(), KernelError> {
@@ -61,8 +62,8 @@ impl<'a> MessagingManager {
         if MAX_BUFFER_SIZE < src_msg.len() {
             return Err(KernelError::BufferOverflow);
         }
-        for tid in 1..MAX_TASKS{
-            let tid_mask = (1 << tid) as u32 ;
+        for tid in 1..MAX_TASKS {
+            let tid_mask = (1 << tid) as u32;
             if tasks_mask & tid_mask == tid_mask {
                 for i in 0..src_msg.len() {
                     self.tcb_table[tid].dest_buffer[i] = src_msg[i];
@@ -74,21 +75,24 @@ impl<'a> MessagingManager {
     }
 
     pub fn receive(&'a mut self, sem_id: SemaphoreId) -> Option<&'a [u32]> {
-            let rt = get_RT();
-            let tcb = &self.tcb_table[rt];
-            match self.msg_scb_table.test_and_reset(sem_id) {
-                Ok(res) if res == true => {
-                    return Some(&tcb.dest_buffer[0..tcb.msg_size])
-                }
-                _ => return None,
-            }
-            return None
+        let rt = get_RT();
+        let tcb = &self.tcb_table[rt];
+        match self.msg_scb_table.test_and_reset(sem_id) {
+            Ok(res) if res == true => return Some(&tcb.dest_buffer[0..tcb.msg_size]),
+            _ => return None,
+        }
+        return None;
     }
 
-    pub fn create(&mut self, tasks: &[u32], receivers: &[u32], src_buffer: StaticBuffer) -> Result<SemaphoreId,KernelError> {
-            let sem_id = self.msg_scb_table.new(generate_task_mask(tasks))?;
-            self.mcb_table[sem_id].src_buffer = src_buffer;
-            self.mcb_table[sem_id].receivers |= generate_task_mask(receivers);
-            return Ok(sem_id);
+    pub fn create(
+        &mut self,
+        tasks: &[u32],
+        receivers: &[u32],
+        src_buffer: StaticBuffer,
+    ) -> Result<SemaphoreId, KernelError> {
+        let sem_id = self.msg_scb_table.new(generate_task_mask(tasks))?;
+        self.mcb_table[sem_id].src_buffer = src_buffer;
+        self.mcb_table[sem_id].receivers |= generate_task_mask(receivers);
+        return Ok(sem_id);
     }
 }
--- src/resources.rs
+++ src/resources.rs
@@ -1,16 +1,17 @@
 use core::cell::RefCell;
-use cortex_m::interrupt::Mutex;
 use cortex_m::interrupt::free as execute_critical;
+use cortex_m::interrupt::Mutex;
 
-use crate::kernel::resource_management::{ResourceManager, ResourceId};
-use crate::helper::generate_task_mask;
 use crate::errors::KernelError;
+use crate::helper::generate_task_mask;
+use crate::kernel::resource_management::{ResourceId, ResourceManager};
 
-lazy_static!{
-    static ref Resources: Mutex<RefCell<ResourceManager>> = Mutex::new(RefCell::new(ResourceManager::init()));
+lazy_static! {
+    static ref Resources: Mutex<RefCell<ResourceManager>> =
+        Mutex::new(RefCell::new(ResourceManager::init()));
 }
 
-pub fn new(tasks: &[u32])  -> Result<ResourceId,KernelError> {
+pub fn new(tasks: &[u32]) -> Result<ResourceId, KernelError> {
     execute_critical(|cs_token| {
         Resources
             .borrow(cs_token)
@@ -20,19 +21,9 @@ pub fn new(tasks: &[u32])  -> Result<ResourceId,KernelError> {
 }
 
 pub fn lock(id: ResourceId) {
-    execute_critical(|cs_token| {
-        Resources
-            .borrow(cs_token)
-            .borrow_mut()
-            .lock(id)
-    })
+    execute_critical(|cs_token| Resources.borrow(cs_token).borrow_mut().lock(id))
 }
 
 pub fn unlock(id: ResourceId) {
-    execute_critical(|cs_token| {
-        Resources
-            .borrow(cs_token)
-            .borrow_mut()
-            .unlock(id)
-    })
+    execute_critical(|cs_token| Resources.borrow(cs_token).borrow_mut().unlock(id))
 }
