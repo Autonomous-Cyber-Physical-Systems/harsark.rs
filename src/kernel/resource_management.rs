ffe6d473077e5ffc0a48cc296b2e38c7c1ad159c
kanishkarj <kanishkarj@hotmail.com>
2019-10-07 22:53:22 +0530

    Resource management optimizations

--- src/kernel/resource_management.rs
+++ src/kernel/resource_management.rs
@@ -12,18 +12,37 @@ use crate::kernel::types::ResourceId;
 const PI: u32 = 0;
 
 #[derive(Clone, Copy)]
+pub struct ResourceControlBlock {
+    rt_ceiling: u32,
+    tasks_mask: u32
+}
+
+#[derive(Clone, Copy)]
 pub struct ResourceManager {
-    resources_block: [u32; MAX_RESOURCES], // Resource Control Block, holds u32 expressing which tasks have access to it.
+    resources_block: [ResourceControlBlock; MAX_RESOURCES], // Resource Control Block, holds u32 expressing which tasks have access to it.
     top: usize,
     pi_stack: [u32; MAX_RESOURCES],
     curr: usize, // used to track current no. of resources initialized
     system_ceiling: u32,
 }
 
+impl ResourceControlBlock {
+    pub const fn new() -> Self {
+        Self {
+            rt_ceiling: PI,
+            tasks_mask: PI
+        }
+    }
+    pub fn set(&mut self, tasks_mask: u32) {
+        self.tasks_mask = tasks_mask;
+        self.rt_ceiling = get_msb(&tasks_mask) as u32;
+    }
+}
+
 impl ResourceManager {
     pub const fn new() -> Self {
         ResourceManager {
-            resources_block: [PI; MAX_RESOURCES],
+            resources_block: [ResourceControlBlock::new(); MAX_RESOURCES],
             top: 0,
             pi_stack: [0; MAX_RESOURCES],
             curr: 0,
@@ -31,30 +50,27 @@ impl ResourceManager {
         }
     }
 
-    pub fn create(&mut self, tasks_mask: &u32) -> Result<ResourceId, KernelError> {
+    pub fn create(&mut self, tasks_mask: u32) -> Result<ResourceId, KernelError> {
         let id = self.curr;
         if id >= MAX_RESOURCES {
             return Err(KernelError::LimitExceeded);
         }
-        self.resources_block[id] = get_msb(&tasks_mask) as u32;
+        self.resources_block[id].set(tasks_mask);
         self.curr += 1;
         Ok(id)
     }
 
     pub fn lock(&mut self, id: ResourceId) -> bool {
-        let rt_ceiling = self.resources_block[id];
+        let resource = &self.resources_block[id];
         let curr_pid = get_pid();
+        let rt_ceiling = resource.rt_ceiling;
         if rt_ceiling > self.system_ceiling {
             self.push_stack(rt_ceiling);
 
-            // can be optimized
-            let mut mask = 0;
-            for i in 0..(rt_ceiling - 1) {
-                mask |= 1 << i;
-            }
+            let mut mask = 1<<(rt_ceiling+1) - 1;
             mask &= !(1<<curr_pid);
         
-            self.system_ceiling = self.resources_block[id];
+            self.system_ceiling = self.resources_block[id].rt_ceiling;
             block_tasks(mask);
             return true;
         }
@@ -62,13 +78,10 @@ impl ResourceManager {
     }
 
     pub fn unlock(&mut self, id: ResourceId) {
-        let rt_ceiling = self.resources_block[id];
-        if rt_ceiling == self.system_ceiling {
+        let resource = self.resources_block[id];
+        if resource.rt_ceiling == self.system_ceiling {
             self.pop_stack();
-            let mut mask = 0;
-            for i in 0..(rt_ceiling - 1) {
-                mask &= 1 << i;
-            }
+            let mut mask = 1<<(resource.rt_ceiling+1) - 1;
             unblock_tasks(mask);
             schedule();
         }
