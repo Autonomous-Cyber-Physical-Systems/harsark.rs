d1e2ee532d11bfc0b97038a01698957d9af7c336
kanishkarj <kanishkarj@hotmail.com>
2019-10-18 14:34:48 +0530

    File split

---
+++ src/config.rs
@@ -0,0 +1,13 @@
+pub const MAX_TASKS: usize = 32;
+pub const MAX_RESOURCES: usize = 32;
+pub const SEMAPHORE_COUNT: usize = 32;
+pub const MCB_COUNT: usize = 32;
+pub const MAX_BUFFER_SIZE: usize = 32;
+pub const EVENT_NO: usize = 32;
+pub const EVENT_INDEX_TABLE_COUNT: usize = 8;
+pub const MAX_STACK_SIZE: usize = 256;
+
+pub const OPCODE_SIGNAL: u8 = 1;
+pub const OPCODE_SEND_MSG: u8 = 1 << 1;
+pub const OPCODE_RELEASE: u8 = 1 << 2;
+pub const OPCODE_ENABLE_EVENT: u8 = 1 << 3;
