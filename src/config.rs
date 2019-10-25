999f514ca5544d316992d875006541a2f92db742
kanishkarj <kanishkarj@hotmail.com>
2019-10-25 23:41:48 +0530

    Resources error fixed
    An important point to note, the error was because of the small stack size. hence stack size should be kept user configurable.

--- src/config.rs
+++ src/config.rs
@@ -5,7 +5,7 @@ pub const MCB_COUNT: usize = 32;
 pub const MAX_BUFFER_SIZE: usize = 32;
 pub const EVENT_NO: usize = 32;
 pub const EVENT_INDEX_TABLE_COUNT: usize = 8;
-pub const MAX_STACK_SIZE: usize = 256;
+pub const MAX_STACK_SIZE: usize = 400;
 
 pub const OPCODE_SIGNAL: u8 = 1;
 pub const OPCODE_SEND_MSG: u8 = 1 << 1;
