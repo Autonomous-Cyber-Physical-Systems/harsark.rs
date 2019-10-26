5f43962decd1d910021cfc333cc0ab53ab45f498
kanishkarj <kanishkarj@hotmail.com>
2019-10-27 01:32:53 +0530

    Configure messages count

--- src/internals/messaging.rs
+++ src/internals/messaging.rs
@@ -1,28 +1,66 @@
 #![feature(const_fn)]
 //use core::alloc::
-use crate::config::{MAX_BUFFER_SIZE, MAX_TASKS, SEMAPHORE_COUNT};
+use crate::config::{MAX_BUFFER_SIZE, MAX_TASKS, MESSAGE_COUNT};
 use crate::errors::KernelError;
-use crate::internals::semaphores::{SemaphoreControlBlock, SemaphoresTable};
+use crate::internals::semaphores::{SemaphoreControlBlock};
 
 use cortex_m_semihosting::hprintln;
 
-use crate::internals::types::MessageId;
+use crate::internals::types::{MessageId, SemaphoreId};
 
 #[derive(Clone, Copy)]
 pub struct MCB {
     pub receivers: u32,
 }
 
-#[derive(Clone, Copy)]
 pub struct MessagingManager {
-    pub mcb_table: [MCB; SEMAPHORE_COUNT],
+    pub mcb_table: [MCB; MESSAGE_COUNT],
     pub msg_scb_table: SemaphoresTable,
+}
+
+pub struct SemaphoresTable {
+    table: [SemaphoreControlBlock; MESSAGE_COUNT],
+    curr: usize,
+}
+
+impl SemaphoresTable {
+    pub const fn new() -> Self {
+        Self {
+            table: [SemaphoreControlBlock { flags: 0, tasks: 0 }; MESSAGE_COUNT],
+            curr: 0,
+        }
+    }
+    pub fn create(&mut self, task_mask: u32) -> Result<SemaphoreId, KernelError> {
+        if self.curr >= MESSAGE_COUNT {
+            return Err(KernelError::LimitExceeded);
+        }
+        let id = self.curr;
+        self.curr += 1;
+        self.table[id].tasks = task_mask;
+        Ok(id)
+    }
+
+    pub fn signal_and_release(
+        &mut self,
+        sem_id: SemaphoreId,
+        tasks_mask: u32,
+    ) -> Result<u32, KernelError> {
+        self.table[sem_id].signal_and_release(tasks_mask)
+    }
+
+    pub fn test_and_reset(
+        &mut self,
+        sem_id: SemaphoreId,
+        curr_pid: u32,
+    ) -> Result<bool, KernelError> {
+        self.table[sem_id].test_and_reset(curr_pid)
+    }
 }
 
 impl<'a> MessagingManager {
     pub const fn new() -> Self {
         Self {
-            mcb_table: [MCB { receivers: 0 }; SEMAPHORE_COUNT],
+            mcb_table: [MCB { receivers: 0 }; MESSAGE_COUNT],
             msg_scb_table: SemaphoresTable::new(),
         }
     }
