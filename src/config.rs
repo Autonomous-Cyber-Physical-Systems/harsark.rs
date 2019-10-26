5756c05a842e7b51a21617af56f893a2f79dc2e6
kanishkarj <kanishkarj@hotmail.com>
2019-10-27 01:16:00 +0530

    Modularize the config

--- src/config.rs
+++ src/config.rs
@@ -1,20 +1,22 @@
+mod tasks_config {
+    #[cfg(all(any(feature = "tasks_32",feature = "default"), not(any(feature = "tasks_16",feature = "tasks_8"))))]
+    pub const MAX_TASKS: usize = 32;
 
-#[cfg(all(any(feature = "tasks_32",feature = "default"), not(any(feature = "tasks_16",feature = "tasks_8"))))]
-pub const MAX_TASKS: usize = 32;
+    #[cfg(feature = "tasks_16")]
+    pub const MAX_TASKS: usize = 16;
 
-#[cfg(feature = "tasks_16")]
-pub const MAX_TASKS: usize = 16;
+    #[cfg(feature = "tasks_8")]
+    pub const MAX_TASKS: usize = 8;
 
-#[cfg(feature = "tasks_8")]
-pub const MAX_TASKS: usize = 8;
+    #[cfg(any(
+        all(feature = "tasks_32", any(feature = "tasks_16", feature = "tasks_8")),
+        all(feature = "tasks_16", any(feature = "tasks_32", feature = "tasks_8")),
+        all(feature = "tasks_8", any(feature = "tasks_32", feature = "tasks_16")),
+    ))]
+    compile_error!("Features 'tasks_32','tasks_18' and 'tasks_8' are mutually exclusive.");
+}
 
-#[cfg(any(
-    all(feature = "tasks_32", any(feature = "tasks_16", feature = "tasks_8")),
-    all(feature = "tasks_16", any(feature = "tasks_32", feature = "tasks_8")),
-    all(feature = "tasks_8", any(feature = "tasks_32", feature = "tasks_16")),
-))]
-compile_error!("Features 'tasks_32','tasks_18' and 'tasks_8' are mutually exclusive.");
-
+mod resources_config {
 #[cfg(all(any(feature = "resources_32",feature = "default"), not(any(feature = "resources_16",feature = "resources_64"))))]
 pub const MAX_RESOURCES: usize = 32;
 
@@ -30,23 +32,29 @@ pub const MAX_RESOURCES: usize = 8;
     all(feature = "resources_64", any(feature = "resources_32", feature = "resources_16")),
 ))]
 compile_error!("Features 'resources_32','resources_18' and 'resources_64' are mutually exclusive.");
+}
 
-#[cfg(all(any(feature = "semahpores_32",feature = "default"), not(any(feature = "semahpores_16",feature = "semahpores_64"))))]
-pub const SEMAPHORE_COUNT: usize = 32;
+mod semaphores_config {
+    #[cfg(all(any(feature = "semahpores_32",feature = "default"), not(any(feature = "semahpores_16",feature = "semahpores_64"))))]
+    pub const SEMAPHORE_COUNT: usize = 32;
 
-#[cfg(feature = "semahpores_16")]
-pub const SEMAPHORE_COUNT: usize = 16;
+    #[cfg(feature = "semahpores_16")]
+    pub const SEMAPHORE_COUNT: usize = 16;
 
-#[cfg(feature = "semahpores_64")]
-pub const SEMAPHORE_COUNT: usize = 8;
+    #[cfg(feature = "semahpores_64")]
+    pub const SEMAPHORE_COUNT: usize = 8;
 
-#[cfg(any(
-all(feature = "semahpores_32", any(feature = "semahpores_16", feature = "semahpores_64")),
-all(feature = "semahpores_16", any(feature = "semahpores_32", feature = "semahpores_64")),
-all(feature = "semahpores_64", any(feature = "semahpores_32", feature = "semahpores_16")),
-))]
-compile_error!("Features 'semahpores_32','semahpores_18' and 'semahpores_64' are mutually exclusive.");
+    #[cfg(any(
+    all(feature = "semahpores_32", any(feature = "semahpores_16", feature = "semahpores_64")),
+    all(feature = "semahpores_16", any(feature = "semahpores_32", feature = "semahpores_64")),
+    all(feature = "semahpores_64", any(feature = "semahpores_32", feature = "semahpores_16")),
+    ))]
+    compile_error!("Features 'semahpores_32','semahpores_18' and 'semahpores_64' are mutually exclusive.");
+}
 
+pub use semaphores_config::SEMAPHORE_COUNT;
+pub use tasks_config::MAX_TASKS;
+pub use resources_config::MAX_RESOURCES;
 
 pub const MAX_BUFFER_SIZE: usize = 32;
 pub const EVENT_NO: usize = 32;
