4c8c5bae1671848e17f190b7e0f8416e148495e7
kanishkarj <kanishkarj@hotmail.com>
2019-11-01 10:43:47 +0530

    Time Management added

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -1,6 +1,8 @@
 use crate::config::PREEMPT_WAIT;
 use crate::event::sweep_event_table;
 use crate::internals::event_manager::EventTableType;
+use crate::internals::time::TickType;
+use crate::kernel::time::{get_msec_10, tick};
 use crate::process::{is_preemptive, schedule};
 use cortex_m::interrupt::free as execute_critical;
 use cortex_m_rt::exception;
@@ -13,52 +15,28 @@ static mut MIN: u32 = 0;
 // SysTick Exception handler
 #[exception]
 fn SysTick() {
-    execute_critical(|_| {
-        let mut m_sec = unsafe { &mut M_SEC };
-        let mut sec = unsafe { &mut SEC };
-        let mut min = unsafe { &mut MIN };
+    if get_msec_10() == PREEMPT_WAIT && is_preemptive() {
+        schedule();
+    }
 
-        if *m_sec == PREEMPT_WAIT && is_preemptive() {
-            schedule();
-        }
-        *m_sec += 1;
+    sweep_event_table(EventTableType::OnOff);
+    sweep_event_table(EventTableType::MilliSec);
 
-        let mut m_sec_flag = true;
-        let mut sec_flag = false;
-        let mut min_flag = false;
-        let mut hour_flag = false;
-
-        if *m_sec >= 100 {
-            *sec += 1;
-            *m_sec = 0;
-            sec_flag = true;
-        }
-
-        if *sec >= 60 {
-            *min += 1;
-            *sec = 0;
-            min_flag = true;
-        }
-
-        if *min >= 60 {
-            *min = 0;
-            hour_flag = true;
-        }
-
-        sweep_event_table(EventTableType::OnOff);
-        if m_sec_flag {
-            sweep_event_table(EventTableType::MilliSec);
-        }
-        if sec_flag {
+    match tick() {
+        TickType::Hour => {
             sweep_event_table(EventTableType::Sec);
-        }
-        if min_flag {
             sweep_event_table(EventTableType::Min);
-        }
-        if hour_flag {
             sweep_event_table(EventTableType::Hour);
         }
-    });
+        TickType::Min => {
+            sweep_event_table(EventTableType::Sec);
+            sweep_event_table(EventTableType::Min);
+        }
+        TickType::Sec => {
+            sweep_event_table(EventTableType::Sec);
+        }
+        _ => {}
+    }
 }
 
 #[exception]
---
+++ src/kernel/time.rs
@@ -0,0 +1,18 @@
+use crate::internals::time::{TickType, Time};
+use core::cell::RefCell;
+use cortex_m::interrupt::free as execute_critical;
+use cortex_m::interrupt::Mutex;
+
+static CURR_TIME: Mutex<RefCell<Time>> = Mutex::new(RefCell::new(Time::new()));
+
+pub fn tick() -> TickType {
+    execute_critical(|cs_token| CURR_TIME.borrow(cs_token).borrow_mut().tick())
+}
+
+pub fn now() -> Time {
+    execute_critical(|cs_token| CURR_TIME.borrow(cs_token).borrow().clone())
+}
+
+pub fn get_msec_10() -> u32 {
+    execute_critical(|cs_token| CURR_TIME.borrow(cs_token).borrow_mut().m_sec_10)
+}
