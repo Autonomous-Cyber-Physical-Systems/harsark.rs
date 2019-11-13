ef58bb7c6cb1d80f9f11fb2ea21a0e84ee684627
kanishkarj <kanishkarj@hotmail.com>
2019-11-14 02:49:43 +0530

    Examples update

--- examples/qemu-cortex-m4/examples/event.rs
+++ examples/qemu-cortex-m4/examples/event.rs
@@ -16,7 +16,7 @@ use hartex_rust::tasks::*;
 use hartex_rust::types::*;
 use hartex_rust::util::generate_task_mask;
 
-struct app {
+struct AppState {
     sem2: SemaphoreId,
     msg1: Message<[u32; 2]>,
 }
@@ -25,7 +25,7 @@ struct app {
 fn main() -> ! {
     let peripherals = resources::init_peripherals().unwrap();
 
-    let app_inst = app {
+    let app_inst = AppState {
         sem2: semaphores::new(generate_task_mask(&[task2])).unwrap(),
         msg1: messages::new(
             generate_task_mask(&[task3]),
@@ -35,16 +35,32 @@ fn main() -> ! {
         .unwrap(),
     };
 
+    /*
+    Creates a FreeRunning Event that occurs once in every second.
+    The event releases task1 when its counter expires.
+    */
     let e1 = events::new_FreeRunning(true, 1, EventTableType::Sec).unwrap();
     events::set_tasks(e1, generate_task_mask(&[task1]));
 
+    /*
+    Creates a FreeRunning Event that occurs once in every 2 seconds.
+    The event signals semaphore sem2 when its counter expires.
+    */
     let e2 = events::new_FreeRunning(true, 2, EventTableType::Sec).unwrap();
     events::set_semaphore(e2, app_inst.sem2, generate_task_mask(&[task1, task2]));
 
-    let e3 = events::new_FreeRunning(false, 3, EventTableType::Sec).unwrap();
+    /*
+    Creates an OnOff Event.
+    The event broadcasts message msg1 whenever it is dispatched.
+    */
+    let e3 = events::new_OnOff(false).unwrap();
     events::set_message(e3, app_inst.msg1.get_id());
 
-    let e4 = events::new_OnOff(true).unwrap();
+    /*
+    Creates a FreeRunning Event that occurs once in every 3 seconds.
+    The event dispatches event3 when its counter expires.
+    */
+    let e4 = events::new_FreeRunning(true, 3, EventTableType::Sec).unwrap();
     events::set_next_event(e4, e3);
 
     static mut stack1: [u32; 300] = [0; 300];
@@ -53,16 +69,14 @@ fn main() -> ! {
 
     spawn!(task1, 1, stack1, params, app_inst, {
         hprintln!("TASK 1: Enter");
-        if let Ok(x) = semaphores::test_and_reset(params.sem2) {
-            if x {
-                hprintln!("TASK 1: sem2 enabled");
-            }
+        if let Ok(true) = semaphores::test_and_reset(params.sem2) {
+            hprintln!("TASK 1: sem2 enabled");
         }
         hprintln!("TASK 1: End");
     });
     spawn!(task2, 2, stack2, params, app_inst, {
         hprintln!("TASK 2: Enter");
-        if let Ok(_x) = semaphores::test_and_reset(params.sem2) {
+        if let Ok(true) = semaphores::test_and_reset(params.sem2) {
             hprintln!("TASK 2: sem2 enabled");
         }
         hprintln!("TASK 2: End");
--- examples/qemu-cortex-m4/examples/resource.rs
+++ examples/qemu-cortex-m4/examples/resource.rs
@@ -14,33 +14,38 @@ use hartex_rust::tasks::*;
 use hartex_rust::types::*;
 use hartex_rust::util::generate_task_mask;
 
-struct app {
+struct AppState {
+    sem2: SemaphoreId,
     sem3: SemaphoreId,
-    sem4: SemaphoreId,
     res1: Resource<[u32; 3]>,
-    res2: Resource<[u32; 3]>,
+    res2: Resource<[u32; 2]>,
 }
 
 #[entry]
 fn main() -> ! {
     let peripherals = resources::init_peripherals().unwrap();
 
-    let app_inst = app {
+    // app_inst also holds the resource containers res1 and res2.
+    let app_inst = AppState {
+        sem2: semaphores::new(generate_task_mask(&[task2])).unwrap(),
         sem3: semaphores::new(generate_task_mask(&[task3])).unwrap(),
-        sem4: semaphores::new(generate_task_mask(&[task4])).unwrap(),
-        res1: resources::new([1, 2, 3], generate_task_mask(&[task1, task2, task3])).unwrap(),
-        res2: resources::new([4, 5, 6], generate_task_mask(&[task4])).unwrap(),
+        res1: resources::new([1, 2, 3], generate_task_mask(&[task1, task2])).unwrap(),
+        res2: resources::new([4, 5], generate_task_mask(&[task3])).unwrap(),
     };
 
     static mut stack1: [u32; 512] = [0; 512];
     static mut stack2: [u32; 512] = [0; 512];
     static mut stack3: [u32; 512] = [0; 512];
-    static mut stack4: [u32; 512] = [0; 512];
 
     spawn!(task1, 1, stack1, params, app_inst, {
         hprintln!("TASK 1: Enter");
+        // If res1 is free, then the closure passed on is executed on the resource.
         params.res1.acquire(|res| {
             hprintln!("TASK 1 : res1 : {:?}", res);
+            semaphores::signal_and_release(params.sem2, 0);
+            semaphores::signal_and_release(params.sem3, 0);
+            for i in 0..10000 {}
+            hprintln!("TASK 1 : task 2 and 3 dispatched");
         });
         hprintln!("TASK 1: End");
     });
@@ -48,29 +53,19 @@ fn main() -> ! {
         hprintln!("TASK 2: Enter");
         params.res1.acquire(|res| {
             hprintln!("TASK 2 : res1 : {:?}", res);
-            semaphores::signal_and_release(params.sem3, 0);
-            semaphores::signal_and_release(params.sem4, 0);
-            hprintln!("TASK 2 : task 3 and 4 dispatched");
         });
         hprintln!("TASK 2: End");
     });
     spawn!(task3, 3, stack3, params, app_inst, {
         hprintln!("TASK 3: Enter");
-        params.res1.acquire(|res| {
-            hprintln!("TASK 3 : res1 : {:?}", res);
+        params.res2.acquire(|res| {
+            hprintln!("TASK 3 : res2 :  {:?}", res);
         });
         hprintln!("TASK 3: End");
     });
-    spawn!(task4, 4, stack4, params, app_inst, {
-        hprintln!("TASK 4: Enter");
-        params.res2.acquire(|res| {
-            hprintln!("TASK 4 : res2 :  {:?}", res);
-        });
-        hprintln!("TASK 4: End");
-    });
 
     init(true);
-    release(generate_task_mask(&[task1, task2]));
+    release(generate_task_mask(&[task1]));
     start_kernel(
         unsafe { &mut peripherals.access().unwrap().borrow_mut() },
         150_000,
