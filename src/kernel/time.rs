ef2bb9c8470ced2733a74e4445670b17d67cdc81
kanishkarj <kanishkarj@hotmail.com>
2019-11-24 12:17:21 +0530

    Time Management Documenation

--- src/kernel/time.rs
+++ src/kernel/time.rs
@@ -1,3 +1,6 @@
+//! # Time Management Module.
+//! Defines Kernel routines for time management.
+
 use core::cell::RefCell;
 
 use cortex_m::interrupt::free as execute_critical;
@@ -5,16 +8,20 @@ use cortex_m::interrupt::Mutex;
 
 use crate::system::time_manager::{TickType, Time};
 
+/// Global instance of Time manager.
 static CURR_TIME: Mutex<RefCell<Time>> = Mutex::new(RefCell::new(Time::new()));
 
+/// This method updates the Kernel of passing of 10 milliseconds.
 pub fn tick() -> TickType {
     execute_critical(|cs_token| CURR_TIME.borrow(cs_token).borrow_mut().tick())
 }
 
+/// Returns the time elapsed since the starting of the application.
 pub fn now() -> Time {
     execute_critical(|cs_token| CURR_TIME.borrow(cs_token).borrow().clone())
 }
 
+/// Returns the 10 Milliseconds field of current time.
 pub fn get_msec_10() -> u32 {
     execute_critical(|cs_token| CURR_TIME.borrow(cs_token).borrow_mut().m_sec_10)
 }
--- src/system/time_manager.rs
+++ src/system/time_manager.rs
@@ -1,3 +1,8 @@
+//! # Time Manager
+//!
+//! Defines Data-structures which manage Kernel time.
+
+/// This struct represents a time object, i.e., it stores the time format.
 #[derive(Clone, Copy)]
 pub struct Time {
     pub m_sec_10: u32,
@@ -7,6 +12,7 @@ pub struct Time {
     pub day: u32,
 }
 
+/// This enum represents the highest order time that elapsed in a tick.
 pub enum TickType {
     MilliSec10,
     Sec,
@@ -16,6 +22,7 @@ pub enum TickType {
 }
 
 impl Time {
+    /// Returns a new instance of `Time`.
     pub const fn new() -> Self {
         Self {
             m_sec_10: 0,
@@ -26,6 +33,12 @@ impl Time {
         }
     }
 
+    /// A tick updates the Time object’s `m_sec_10` field, which implies 10 MilliSecond has passed,
+    /// and After every tick, the other fields (Second, Minutes, Hour, and Day) of the Time object
+    /// are also updated if required. The tick method is called by the SysTick interrupt handler, and
+    /// the return value is used by the interrupt handler to dispatch events. Note that if returned TickType
+    /// is Hour, it not only implies the current tick caused completion of an hour, but that is caused
+    /// completion of an Hour, Second, and 10 MilliSecond but not a Day.
     pub fn tick(&mut self) -> TickType {
         self.m_sec_10 += 1;
         let mut res = TickType::MilliSec10;
