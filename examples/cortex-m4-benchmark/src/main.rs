ec0dbba980e392266fd870c8297d8bffdbc6151f
kanishkarj <kanishkarj@hotmail.com>
2020-01-27 23:56:48 +0530

    initialize Benchmark setup

---
+++ examples/cortex-m4-benchmark/src/main.rs
@@ -0,0 +1,15 @@
+#![no_std]
+#![no_main]
+
+extern crate panic_halt;
+extern crate stm32f4;
+
+use cortex_m_rt::entry;
+
+#[entry]
+fn main() -> ! {
+    /*
+        Look into examples folders for the examples
+    */
+    loop {}
+}
